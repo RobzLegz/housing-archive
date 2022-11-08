2eba2fc4027c855d2537a9fd640593c173d8b296
RobzLegz <robzlegz@gmail.com>
2022-11-08 14:49:31 +0200

    setup

--- src/components/home/Landing.tsx
+++ src/components/home/Landing.tsx
@@ -1,7 +1,23 @@
+import Image from "next/image";
 import React from "react";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <form className="w-full h-screen flex flex-col items-center justify-center relative">
+      <div className="w-full h-full relative">
+        <Image
+          src="/images/splash-house.jpg"
+          alt="Splash house background"
+          fill
+          draggable={false}
+        />
+      </div>
+
+      <div className="flex w-[95%] max-w-[800px] rounded-lg bg-red-500 p-4 z-50 absolute top-0 left-0 h-80">
+        <input type="text" name="search" id="search" />
+      </div>
+    </form>
+  );
 };
 
 export default Landing;
