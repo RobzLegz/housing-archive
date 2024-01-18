f0cd150a10142d69184fe2f69a850134e091054f
alismanis-lv <97937927+alismanis-lv@users.noreply.github.com>
2024-01-18 17:13:44 +0200

    Update PageModule.tsx
    Meta messneger

--- src/modules/PageModule.tsx
+++ src/modules/PageModule.tsx
@@ -15,6 +15,36 @@ const PageModule: PageComponent<PageModuleProps> = ({
 }) => {
   return (
     <main>
+    <!-- Messenger Chat Plugin Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Chat Plugin code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+      var chatbox = document.getElementById('fb-customer-chat');
+      chatbox.setAttribute("page_id", "129886600213748");
+      chatbox.setAttribute("attribution", "biz_inbox");
+    </script>
+
+    <!-- Your SDK code -->
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          xfbml            : true,
+          version          : 'v18.0'
+        });
+      };
+
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+    </script>
       <Head>
         <title>{`Īpašumu arhīvs${title ? ` | ${title}` : ""}`}</title>
         <meta name="description" content={description} />
