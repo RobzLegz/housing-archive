b2adb3743f5e65757a5858842747ff6027e4b59a
alismanis-lv <97937927+alismanis-lv@users.noreply.github.com>
2024-01-18 18:54:31 +0200

    Update Facebook.tsx

--- src/components/home/Facebook.tsx
+++ src/components/home/Facebook.tsx
@@ -23,7 +23,7 @@ function Facebook() {
               var js, fjs = d.getElementsByTagName(s)[0];
               if (d.getElementById(id)) return;
               js = d.createElement(s); js.id = id;
-              js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
+              js.src = 'https://connect.facebook.net/lv_LV/sdk/xfbml.customerchat.js';
               fjs.parentNode.insertBefore(js, fjs);
             }(document, 'script', 'facebook-jssdk'));
         `}
