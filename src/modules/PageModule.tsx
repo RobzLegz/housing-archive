e3bf2b3ea157c015f523c33d4adc667cdf252aff
alismanis-lv <97937927+alismanis-lv@users.noreply.github.com>
2024-01-18 18:14:36 +0200

    Update PageModule.tsx

--- src/modules/PageModule.tsx
+++ src/modules/PageModule.tsx
@@ -20,44 +20,6 @@ const PageModule: PageComponent<PageModuleProps> = ({
         <meta name="description" content={description} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-    {/* <!-- Messenger Chat Plugin Code --> */}
-    <div id="fb-root"></div>
-
-    {/* <!-- Your Chat Plugin code --> */}
-    <div id="fb-customer-chat" class="fb-customerchat"></div>
-
-        <Script
-          id="messenger-tag"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `var chatbox = document.getElementById('fb-customer-chat');
-                     chatbox.setAttribute("page_id", "129886600213748");
-                     chatbox.setAttribute("attribution", "biz_inbox");`,
-          }}
-        ></Script
-             
-    {/* <!-- Your SDK code --> */}
-        <Script
-          id="messenger-sdk"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v18.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));`,
-          }}
-        ></Script      
       {children}
     </main>
   );
