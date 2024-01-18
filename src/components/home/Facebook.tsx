2a53b72bf71439955e3216452d649ca3927009d9
alismanis-lv <97937927+alismanis-lv@users.noreply.github.com>
2024-01-18 18:18:53 +0200

    Create Facebook.tsx

---
+++ src/components/home/Facebook.tsx
@@ -0,0 +1,35 @@
+import Script from 'next/script';
+
+function Facebook() {
+  return (
+    <div>
+      <div id="fb-root"></div>
+
+      <div id="fb-customer-chat" className="fb-customerchat"></div>
+      <Script strategy="lazyOnload">
+        {`
+            var chatbox = document.getElementById('fb-customer-chat');
+            chatbox.setAttribute("page_id", "129886600213748");
+            chatbox.setAttribute("attribution", "biz_inbox");
+      
+            window.fbAsyncInit = function() {
+              FB.init({
+                xfbml            : true,
+                version          : 'v18.0'
+              });
+            };
+      
+            (function(d, s, id) {
+              var js, fjs = d.getElementsByTagName(s)[0];
+              if (d.getElementById(id)) return;
+              js = d.createElement(s); js.id = id;
+              js.src = 'https://connect.facebook.net/en_GB/sdk/xfbml.customerchat.js';
+              fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        `}
+      </Script>
+    </div>
+  );
+}
+
+export default Facebook;
